59c2eabdced49699d29d6d5f0503dad33d064b3c
Abdullah149081 <masudmd425@gmail.com>
2023-05-09 20:41:09 +0600

    post add

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const follow = require("./data/follow.json");
 
 app.use(cors());
 app.use(express.json());
 
-
 // mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ignmh8y.mongodb.net/?retryWrites=true&w=majority`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +25,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("coffeeDB");
+    const coffeeCollection = database.collection("coffee");
+
+    app.post("/coffee", async (req, res) => {
+      const newCoffee = req.body;
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result)
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -38,6 +46,10 @@ run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Coffee store running ");
+});
+
+app.get("/follow", (req, res) => {
+  res.send(follow);
 });
 
 app.listen(port, () => {
