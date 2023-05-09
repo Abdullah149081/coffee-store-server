91fd46e94bdb1eff4d735f95b9a497b75e422cb9
Abdullah149081 <masudmd425@gmail.com>
2023-05-10 00:41:03 +0600

    update server

--- index.js
+++ index.js
@@ -48,6 +48,27 @@ async function run() {
       res.send(result);
     });
 
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const coffee = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateCoffee = {
+        $set: {
+          name: coffee.name,
+          email: coffee.email,
+          chef: coffee.chef,
+          supplier: coffee.supplier,
+          taste: coffee.taste,
+          category: coffee.category,
+          details: coffee.details,
+          photo: coffee.photo,
+        },
+      };
+      const result = await coffeeCollection.updateOne(filter, updateCoffee, options);
+      res.send(result);
+    });
+
     app.delete("/coffee/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
